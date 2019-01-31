fda278e1aec4fe1eef99cf14a1e38fdfc797c832
Anthony <ajssalemo@gmail.com>
2019-01-30 21:04:46 -0500

    added rest-link

--- src/index.js
+++ src/index.js
@@ -5,17 +5,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { RestLink } from "apollo-link-rest";
 import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloProvider } from 'react-apollo';
 
+// Test
+import { graphql } from 'react-apollo'
+import { gql } from 'apollo-boost'
+
+require('dotenv').config();
+
 // ------------------------------------------------------------------------------------------- //
+const restLink = new RestLink({
+    uri: `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&`,
+});
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://www.omdbapi.com/?apikey=[yourkey]&' }),
+  link: restLink,
   cache: new InMemoryCache()
-})
+});
 
 // ------------------------------------------------------------------------------------------- //
 // ------------------------------------------------------------------------------------------- //
