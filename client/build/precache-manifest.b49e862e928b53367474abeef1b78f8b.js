d436e6b12ee468ea687d899b4e6cf983c12f133a
Anthony Salemo <ajssalemo@gmail.com>
2019-02-28 22:24:04 -0500

    deploying

---
+++ client/build/precache-manifest.b49e862e928b53367474abeef1b78f8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c814064824e2e852ca18",
+    "url": "/static/js/main.c8140648.chunk.js"
+  },
+  {
+    "revision": "8fb01d80dd2360fe2d6b",
+    "url": "/static/js/1.8fb01d80.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "84e4d5c18a17284b2be83396a8e78c73",
+    "url": "/static/media/errorgif.84e4d5c1.gif"
+  },
+  {
+    "revision": "4c6c6ed39717e8c1c9fe65cd67c87125",
+    "url": "/static/media/moviebackground.4c6c6ed3.jpg"
+  },
+  {
+    "revision": "b88801f71d8221dfa321c56f91414383",
+    "url": "/index.html"
+  }
+];
